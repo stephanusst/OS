ac6afe7bc3570504dfb6b59b579c5165707f6663
stephanusst <stephanusst@gmail.com>
2023-03-21 06:42:08 +0700

    Add files via upload

---
+++ helloopenmp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "omp.h"
+
+int main(){
+omp_set_num_threads(4);
+#pragma omp parallel
+	{
+	  int ID=omp_get_thread_num();
+	  printf(" Hello(%d)", ID);
+	  printf(" World(%d)\n",ID);
+	}
+  return 0;
+}
